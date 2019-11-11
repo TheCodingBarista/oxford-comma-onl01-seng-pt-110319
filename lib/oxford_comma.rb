020df8e4c60df4e88d7e7882314dfbc7c32b014e
Melissa Holt <melissa.holt21@gmail.com>
2019-11-11 21:10:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
+  
+case array.length
+
+
+when == 1
+  return "#{array[0]}""
+when == 2 
+  return array.join(" and "")
+when >= 3
+  array[-1]
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  else array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end
+
 
 end
